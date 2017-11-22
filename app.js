b088eeace79fb73b8a5cea5539ac531d59e084fd
Sparkstream <p.quach97@hotmail.com>
2017-11-22 13:05:16 +1300

    Added bot files

---
+++ app.js
@@ -0,0 +1,35 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+//Session state
+var lastSaid = "";
+
+
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    
+    if(lastSaid==session.message.text){
+        session.send("You said that already!");
+    }else{
+        session.send("You said: %s", session.message.text); //%s is replaced with session.message.text which is the user input
+    }
+   
+    lastSaid = session.message.text;
+
+});
